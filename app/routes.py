cc4332a7685c944d63e972ef1d84656ada5d8369
seanald <seanald@protonmail.com>
2020-01-26 11:15:18 -0800

    added '/index' route (equivalent to '/'), added <head> sections

--- app/routes.py
+++ app/routes.py
@@ -2,9 +2,29 @@ from app import app
 
 #home page
 @app.route("/")
+@app.route("/index")
 def home():
-    return "<h1>My Blog</h1>"
+    user = {'username': 'Sean'}
+#    return "<h1>My Blog</h1>"
+    return '''\
+<html>
+    <head>
+        <title>Home Page - Blog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</head>'''
 
 @app.route("/about")
 def about():
-    return "<h1>All about me</h1>"
+    return '''\
+<html>
+    <head>
+        <title>About - Blog</title>
+    </head>
+    <body>
+        <h1>All about me</h1>
+        <p>Laboriosam est sed deleniti porro quis. Et nostrum minus distinctio quasi. Et delectus ipsum harum perspiciatis veniam nihil similique eos. Qui consequatur repellendus voluptas ad sunt. Velit enim culpa ea qui blanditiis corporis. Libero sapiente iste non dolorem.</p>
+    </body>
+</html>'''
