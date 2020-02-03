b26ea4fbf3ddff8636e2f5a38e52613c00cee44a
seanald <vulcan@olympus>
2020-02-03 10:18:46 -0800

    Added page editing through /edit/{page} URLs

--- app/blog_helpers.py
+++ app/blog_helpers.py
@@ -1,16 +1,28 @@
 from markdown import markdown
-import os
-def render_markdown(file_name, dir_path = 'app/views'):
-    html = ""
-    path = os.path.join(dir_path, file_name)
-    with open(path) as html_file:
-        html = html_file.read()
-        html = markdown(html)
-    return html
+import os, sqlite3
 
-def render_html(file_name, dir_path = 'app/views'):
-    html = ""
+def render_markdown(file_name, dir_path = 'app/views'):
+    return markdown(read_txt(file_name, dir_path))
+
+def read_txt(file_name, dir_path = 'app/views'):
+    txt = ''
     path = os.path.join(dir_path, file_name)
-    with open(path) as html_file:
-        html = html_file.read()
-    return html
+    with open(path) as txt_file:
+        txt = txt_file.read()
+    return txt
+
+def sql_execute(sql, *parameters):
+    return sqlite3.connect('blog.db').cursor().execute(sql, parameters).fetchall()
+
+def is_admin(user):
+    if sql_execute('SELECT admin FROM users WHERE name=?', user)[0][0]:
+        return True
+    return False
+
+def is_view(view_name = '', dir_path = 'app/views'):
+    print(view_name)
+    print(os.path.join(dir_path, os.path.normpath(view_name)))
+    if os.path.isfile(os.path.join(dir_path, os.path.normpath(view_name))):
+        return True
+    else:
+        return False
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,10 @@
 from app import app
 from markdown import markdown
-from flask import render_template_string, request, session, redirect, url_for, escape
-from app.blog_helpers import render_markdown, render_html
+from flask import render_template_string, request, session, redirect, url_for, escape, abort
+from app.blog_helpers import render_markdown, read_txt, sql_execute, is_admin, is_view
 import os, sqlite3, secrets, hashlib
 
+# What happens if this is generated randomly here?
 app.secret_key = '`(7h_B/G PH:=IyT-$L^mE~5AR!Y|?/;i=2z1]ESGMKRtg-f'
 
 
@@ -11,31 +12,59 @@ app.secret_key = '`(7h_B/G PH:=IyT-$L^mE~5AR!Y|?/;i=2z1]ESGMKRtg-f'
 def login():
     if request.method == 'POST':
         # Get user record from SQLite database user.db
-        conn = sqlite3.connect('user.db')
-        udata = conn.cursor().execute('SELECT * FROM users WHERE name=?', (request.form['username'],)).fetchall()
+        udata = sql_execute('SELECT * FROM users WHERE name=?', request.form['username'])
 
         # Check that 1 matching record was found AND check hash from db against hash of salt + provided password
         if len(udata) == 1 and udata[0][3] == hashlib.sha256((udata[0][2] + request.form['password']).encode()).hexdigest():
             session['username'] = request.form['username']
             return redirect(url_for('index'))
         else:
-            return render_html('login.html') + '\n<p><font color="red">invalid credentials</font></p>'
+            return read_txt('login.html') + '\n<p><font color="red">invalid credentials</font></p>'
 
-    return render_html('login.html')
+    return render_page('login')
 
 @app.route('/logout')
 def logout():
     session.pop('username', None)
     return redirect(url_for('index'))
 
+@app.route('/')
 @app.route('/index')
 def index():
     return render_page('index')
-    
+
+@app.route('/blog/<view_name>')
+def render_blog(view_name):
+    render_page('blog/view_name')
+
 # generic page
-@app.route("/<view_name>")
-def render_page(view_name):
-    html = render_markdown(view_name + '.md')
+@app.route('/<view_name>')
+def render_page(view_name = '', head = read_txt('bar.html'), foot = ''):
+    html = head
+    if is_view(view_name + '.md'):
+        print('load: ' + view_name + '.md')
+        html += '\n' + render_markdown(view_name + '.md')
+    elif is_view(view_name + '.html'):
+        print('load: ' + view_name + '.html')
+        html += '\n' + read_txt(view_name + '.html')
+    else:
+        abort(404)
+    html += '\n' + foot
+    login = 'Not logged in.'
     if session:
-        html += 'logged in as: ' + session['username']
-    return render_template_string(html, view_name = view_name)
+        login = 'Logged in as: ' + session['username']
+    return render_template_string(html, view_name = view_name, login = login)
+
+# edit generic page
+@app.route('/edit/<view_name>')
+def edit_page(view_name):
+    if session and is_admin(session['username']):
+        html = render_page('edit')
+        if os.path.isfile(view_name + '.md'):
+            view_name += '.md'
+        elif os.path.isfile(view_name + '.html'):
+            view_name += '.html'
+        else:
+            view_name = ''
+        page = read_txt(view_name)
+        return render_template_string(html, view_name = view_name, page = page)
